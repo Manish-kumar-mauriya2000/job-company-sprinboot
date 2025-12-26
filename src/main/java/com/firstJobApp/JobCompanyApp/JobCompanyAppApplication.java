97ab1187ca44d15db17dfc83436726538b0365cd
Manish-kumar-mauriya2000 <manishkumarmauriya2000@gmail.com>
2025-12-26 21:07:17 +0530

    Reapply "Initial commit"
    This reverts commit 8ac126e5fc20459ad2598545e9f4d01fef904c41.

---
+++ src/main/java/com/firstJobApp/JobCompanyApp/JobCompanyAppApplication.java
@@ -0,0 +1,13 @@
+package com.firstJobApp.JobCompanyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCompanyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCompanyAppApplication.class, args);
+	}
+
+}
